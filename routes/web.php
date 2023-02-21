5ca5818688e54698e48f1893def2c4a566b32ceb
Mohamed KB <mohabel98@gmail,com>
2023-02-21 19:24:29 +0100

    controlador publico hecho

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name();
+Route::get('/', [PublicController::class, 'welcome'])->name('welcome');
