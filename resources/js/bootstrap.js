f5e5b3b97c44f33eb10f1406b02019f39d37f8e6
Mohamed KB <mohabel98@gmail,com>
2023-02-21 19:46:20 +0100

    controlador publico hecho

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,6 +3,9 @@
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
+import '../css/app.css'
+
+import * as bootstrap from 'bootstrap'
 
 import axios from 'axios';
 window.axios = axios;
@@ -10,8 +13,7 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 
-import '../css/app.css';
-import * as bootstrap from 'bootstrap';
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
